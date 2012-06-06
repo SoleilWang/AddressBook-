b545321ab561dbcc45bc8f1d8e56aa2bb75bb286
ewyabdl <alex.d.wang@ericsson.com>
2012-06-06 09:32:35 +0800

    add  AddressBookException(String message), AddressBookException(Throwable cause) constructor

--- addressbook/src/main/java/com/alex/wang/addressbook/exception/AddressBookException.java
+++ addressbook/src/main/java/com/alex/wang/addressbook/exception/AddressBookException.java
@@ -18,12 +18,38 @@ public class AddressBookException extends Exception {
     }
 
     /**
-     * Constructs a new exception with the specified detail message and cause.
+     * Constructs a new AddressBookException with the specified detail message.
      * 
      * @param message
-     * @param exception
+     *            the detail message.
      */
-    public AddressBookException(String message, Throwable exception) {
-        super(message, exception);
+    public AddressBookException(String message) {
+        super(message);
     }
+
+    /**
+     * Constructs a new AddressBookException with the specified detail message and cause.
+     * 
+     * @param message
+     *            the detail message.
+     * @param cause
+     *            the cause (which is saved for later retrieval by the Throwable.getCause() method). (A null value is permitted, and indicates that the cause is
+     *            nonexistent or unknown.)
+     */
+    public AddressBookException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new AddressBookException with the specified cause and a detail message of (cause==null ? null : cause.toString()) (which typically contains
+     * the class and detail message of cause).
+     * 
+     * @param cause
+     *            the cause (which is saved for later retrieval by the Throwable.getCause() method). (A null value is permitted, and indicates that the cause is
+     *            nonexistent or unknown.)
+     */
+    public AddressBookException(Throwable cause) {
+        super(cause);
+    }
+
 }
